30bb6a37aa947b3d4670ae9941653451365eba64
Steven Ly <1stevenly92@gmail.com>
2020-09-27 00:31:58 -0700

    working on implementing redux

--- src/component/Headlines.js
+++ src/component/Headlines.js
@@ -1,45 +1,59 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { makeApiCall } from '../actions'
 
-makeApiCall = () => {
-    fetch (`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`)
-    .then(response => response.json())
-    .then(
-        (jsonifiedResponse) => {
-            this.setState({
-                isloaded:true,
-                headlines:jsonifiedResponse.results
-            });
-        })
-        .catch((error) => {
-            this.setState({
-                isloaded:true,
-                error
-            })
-        })
-}
 
 
 
 
 
 class Headlines extends React.Component {
-    constructor(props) {
-        super(props) 
-            this.state = {
-                error:null,
-                isLoaded:false,
-                headlines:[]
-            }
-        
+
+    componentDidMount() {
+        const { dispatch } = this.props;
+        dispatch(makeApiCall());
     }
 
-    render(){
-        return(
-            <h1>HI</h1>
-        )
+
+
+
+
+    render() {
+        const { error, isLoaded, headlines } = this.state
+        if (error) {
+            return <React.Fragment>Error:{error.message}</React.Fragment>
+        } else if (!isLoaded) {
+            return <React.Fragment>Loading...</React.Fragment>
+        } else {
+            return (
+            <React.Fragment>
+                <h1>Headlines</h1>
+                <ul>
+                    {headlines.map((headline, index) =>
+                    <li key={index}>
+                        <h3>{headline.title}</h3>
+                        <p>{headline.abstract}</p>
+                    </li>
+            )}
+                </ul>
+            </React.Fragment>
+            )
+
+        }
+
 
 
 
 
     }
 }
+
+const mapStateToProps = state => {
+    return {
+        headlines: state.headlines,
+        isLoading: state.isLoading,
+        error: state.error
+    }
+}
+
+export default connect(mapStateToProps)(Headlines);
