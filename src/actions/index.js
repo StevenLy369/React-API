b074ea6abde18771ac424ae2fa77632d6fe1a800
Steven Ly <1stevenly92@gmail.com>
2020-09-28 13:26:31 -0700

    finished testing actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -14,16 +14,3 @@ export const getHeadlinesFailure = (error) => ({
 	error
 });
 
-export const makeApiCall = () => {
-	return (dispatch) => {
-		dispatch(requestHeadlines);
-		return fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`)
-			.then((response) => response.json())
-			.then((jsonifiedResponse) => {
-				dispatch(getHeadlinesSuccess(jsonifiedResponse.results));
-			})
-			.catch((error) => {
-				dispatch(getHeadlinesFailure(error));
-			});
-	};
-};
